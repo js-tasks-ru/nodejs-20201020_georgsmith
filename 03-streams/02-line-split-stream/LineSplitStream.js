f3decb4e1afe1bc0d40d5f62131e60f964ed2450
GeorgiyK <georgiyk@scholarshipowl.com>
2020-11-07 22:54:19 +0200

    Add LineSplitStream transform stream, which separates the text with a delimiter

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -2,14 +2,26 @@ const stream = require('stream');
 const os = require('os');
 
 class LineSplitStream extends stream.Transform {
-  constructor(options) {
+  constructor({ delimiter, ...options }) {
     super(options);
+    this.stringChunkAccumulator = "";
+    this.delimiter = delimiter || os.EOL;
   }
 
   _transform(chunk, encoding, callback) {
+    this.stringChunkAccumulator += chunk.toString("utf8");
+
+    if (this.stringChunkAccumulator.includes(this.delimiter)) {
+      const lines = this.stringChunkAccumulator.split(this.delimiter);
+      this.stringChunkAccumulator = lines.pop();
+      lines.forEach(line => this.push(line));
+    }
+
+    callback();
   }
 
   _flush(callback) {
+    callback(null, this.stringChunkAccumulator);
   }
 }
 
