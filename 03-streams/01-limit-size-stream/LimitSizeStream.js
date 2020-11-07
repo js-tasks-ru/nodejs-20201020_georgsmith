9035fb72e24bf5e9a693431ed03068296708224b
GeorgiyK <georgiyk@scholarshipowl.com>
2020-11-07 21:29:07 +0200

    Create LimitSizeStream class to limit transmitted data

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -2,12 +2,22 @@ const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
-  constructor(options) {
+  constructor({ limit, ...options }) {
+    if(!limit || typeof limit !== "number")
+        throw Error("Please provide corect limit option");
     super(options);
+    this.limit = limit;
+    this.transferedDataSize = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    this.transferedDataSize += chunk.length;
 
+    if(this.transferedDataSize > this.limit) {
+      callback(new LimitExceededError());
+    }
+
+    callback(null, chunk);
   }
 }
 
