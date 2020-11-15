622922ed8324819ea7e24de0824e310e38274b74
GeorgiyK <georgiyk@scholarshipowl.com>
2020-11-15 21:37:11 +0200

    Implement returning file by API request

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -1,16 +1,47 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
+  let pathname = url.parse(req.url).pathname.slice(1);
+
+  try {
+    pathname = decodeURIComponent(pathname);
+  } catch(err) {
+    res.statusCode = 400;
+    res.end("Bad request");
+  }
+
+  if(pathname.includes("\0") || pathname.includes("/")) {
+    res.statusCode = 400;
+    res.end("Bad request");
+  }
 
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'GET':
+      const rs = fs.createReadStream(filepath);
+
+      rs.on("error", (err) => {
+        if(err.code === "ENOENT") {
+          res.statusCode = 404;
+          res.end("File not found");
+        } else {
+          res.statusCode = 500;
+          res.end("Server error");
+        }
+      });
+
+      rs.on("end", () => {
+        res.statusCode = 200;
+        res.end();
+      })
+
+      rs.pipe(res);
 
       break;
 
